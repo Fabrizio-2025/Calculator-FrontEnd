c1aae1df791a1f59a9a09594cf0a1a21c388afa2
Fabrizio-2025 <fabriziobc2020@outlook.com>
2022-12-25 14:00:38 -0500

    Feat: First commit for calculator FrontEnd

---
+++ Operation.js
@@ -0,0 +1,30 @@
+function operator() 
+{
+    var numer1=parseInt(document.getElementById('n1').value);
+    var numer2=parseInt(document.getElementById('n2').value);
+    var box = document.getElementById('OperationName');
+    var option = box.options[box.selectedIndex].text;
+    
+    if(option == "Addition")
+    {
+    var answer = numer1+numer2;
+    document.getElementById('answerText').innerHTML=answer;
+    }
+    if(option == "Subtraction")
+    {
+    var answer = numer1-numer2;
+    document.getElementById('answerText').innerHTML=answer;
+    }
+    if(option == "Multiplication")
+    {
+    var answer = numer1*numer2;
+    document.getElementById('answerText').innerHTML=answer;
+    }
+    if(option == "Division")
+    {
+    var answer = numer1/numer2;
+    document.getElementById('answerText').innerHTML=answer;
+    }
+    
+    
+}
